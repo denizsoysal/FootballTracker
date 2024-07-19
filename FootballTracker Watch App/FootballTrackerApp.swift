43467de1e80ce5c65c7c86d3f9dc0a451ff34b17
user266481 <user266481@#55.60>
2024-07-19 17:53:50 +0200

    Initial Commit

---
+++ FootballTracker Watch App/FootballTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTrackerApp.swift
+//  FootballTracker Watch App
+//
+//  Created by user266481 on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
